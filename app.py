538f16fa41bb4c95560cbb685c4d8e6c31b5d62e
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-05-16 13:38:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def calculation(n):
     digits_num = len(str(n))
     new_num = n
     while True:
-        s = list(str(new_num).zfill(4))
+        s = list(str(new_num).zfill(digits_num))
         
         big = int(''.join(sorted(s, reverse=True)))
         small = int(''.join(sorted(s)))
@@ -25,5 +25,5 @@ ini_numer = st.sidebar.number_input('Input a number:', value=2048)
 l = calculation(ini_numer)
 st.write(f'The initial number is {l[0][-1]}')
 for i in l[1:]:
-    st.write(f'{i[0]} - {i1[1]} = {i[2]}')
-st.write(f'{l[-1][-1]}')
+    st.write(f'{i[0]} - {i[1]} = {i[2]}')
+st.write(f'End in {l[-1][-1]}')
